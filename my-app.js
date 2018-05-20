b8dff2ec73b59d43a5a58ba1842099575362c172
manick <manick@affintyanswers.com>
2018-05-20 15:21:59 +0530

    Set start script and dynamic port

--- my-app.js
+++ my-app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs');
@@ -64,4 +67,4 @@ app.get('/error',(req, res)=> {
   res.send('<h1>Unknown Error<h1>');
 });
 
-app.listen(3000);
+app.listen(port, () => {console.log(`Server is up on ${port}`);});
